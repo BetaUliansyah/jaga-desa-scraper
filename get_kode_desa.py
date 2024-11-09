b0a38922732d47984b711618fa3a99d4d83f4907
Beta Uliansyah <beta.uliansyah@gmail.com>
2024-11-09 16:52:47 +0700

    Update get_kode_desa.py

--- get_kode_desa.py
+++ get_kode_desa.py
@@ -1,20 +1,28 @@
 import requests
-requests.packages.urllib3.disable_warnings() 
-from bs4 import BeautifulSoup
 import pandas as pd
 import json
 from time import perf_counter
+
+# Mulai pencatatan waktu
 start_time = perf_counter()
-print('Bismillah. Perkiraan berjalan 11 menit. Harap tunggu.')
+print('Bismillah. Perkiraan berjalan 10 detik. Harap tunggu.')
+
+# Mematikan peringatan SSL
+requests.packages.urllib3.disable_warnings() 
+
+# Mengirimkan permintaan dengan session yang sama
 s = requests.Session()
 r = s.get('https://jaga.id/api/v5/desa/search?limit=1000000&offset=0&nama=&kota=', verify=False)
-response_json = json.loads(r.text)
-data_df = pd.DataFrame()
-for i in response_json['data']['result']:
-    row_df = pd.DataFrame(i, index=[0])
-    data_df = pd.concat([data_df, row_df], ignore_index=True)
-data_df.sort_values(by=['id'])
+response_json = r.json()  # Langsung memparsing JSON
+
+# Membuat DataFrame langsung dari data JSON
+data_df = pd.DataFrame.from_records(response_json['data']['result'])
+
+# Mengurutkan dan menyimpan ke file CSV
+data_df.sort_values(by=['id'], inplace=True)
 data_df.to_csv('data_desa_jaga.csv', index=False)
+
+# Menghitung waktu dan menampilkan hasil
 end_time = perf_counter()
 total_time = end_time - start_time
 rows = len(data_df)
